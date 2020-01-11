b72339409004d3feb0d9e677c02ca7f7425df1ed
PeTeR0316 <49382552+PeTeR0316@users.noreply.github.com>
2020-01-11 23:49:15 +0900

    Add files via upload

---
+++ .github/test1.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<link rel="stylesheet" href="test1.css">
+</head>
+<body>
+<div class="navbar">
+  <div class="logo">
+    <img src=images/univers/apple-icon-180x180.png>
+  </div>
+  <div>
+    <ul>
+      <li><span>LATEST</span></li>
+      <li>MAN</li>
+      <li>WOMAN</li>
+      <li>KIDS</li>
+    </ul>
+    <div class="menu-icon">
+      <img src="images/univers/apple-icon-180x180.png">
+      <img src="images/univers/apple-icon-180x180.png">
+    </div>
+  </div>
+</div>
+<div class="banner">
+  <div class="left-col">
+    <div class="banner-text">
+      <h6>RECENT POST</h6>
+      <h1>Elegance is elimination</h1>
+      <p>Style is something</p>
+      <h4>EXPLORE</h4>
+    </div>
+    <div class="small-img">
+      <p>It is a long esttablished fact that a reader will be
+        disstracted bt the readavle content of a page when looking
+        at its
+        <span>MORE</span>
+      </p>
+      <img src="images/univers/apple-icon-180x180.png">
+    </div>
+    <div class="black-line"><span></span>
+    <div class="vertical-menu">
+      <ul>
+        <li>1월</li>
+        <li>2월</li>
+        <li>3월</li>
+        <li><span>4월</span></li>
+      </ul>
+    </div>
+  </div>
+    <div class="right-col">
+      <div class="black-line"><span></span>
+      </div>
+      <img src="images/univers/apple-icon-180x180.png">
+      <div class="social-icons">
+        <img src="images/univers/apple-icon-180x180.png">
+        <img src="images/univers/apple-icon-180x180.png">
+        <img src="images/univers/apple-icon-180x180.png">
+        <img src="images/univers/apple-icon-180x180.png">
+    </div>
+  </div>
+</body>
+</html>
